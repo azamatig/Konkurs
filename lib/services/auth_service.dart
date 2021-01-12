f36b30670627e28996f5ba5a433bd0b5214cc5a2
Azamat Tursunov <azamat.tursunov@uco.kz>
2021-01-12 18:44:13 +0600

    auth fix

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -10,6 +10,8 @@ class AuthService {
 
   static void signUpUser(
       BuildContext context, String name, String email, String password) async {
+    var placeholder =
+        'https://lh3.googleusercontent.com/proxy/keRqNlAaDt-yVRvFlc9RTITCilT5T7mep3tmcELK67d0dG1eEcKbGttr0TV6Zhrd3eGZ71ndBBkPt5FekS5qyQn9eqPIb4xuo6cHEL5PC-o0S5i2OmuddBK_wEMepSiB13-VMLSqLYjyW_IdONAhE4wb-723MeifJzyklZlUaZlLgrBfIxifvTbpV10UEnc09GP4r463OodDujkISbKJvh-F0ZE';
     try {
       AuthResult authResult = await _auth.createUserWithEmailAndPassword(
         email: email,
@@ -20,7 +22,10 @@ class AuthService {
         _firestore.collection('/users').doc(signedInUser.uid).set({
           'name': name,
           'email': email,
-          'profileImageUrl': '',
+          'profileImageUrl': placeholder,
+          'bio': 'Напишите о себе',
+          'age': 'Возраст',
+          'location': 'Местонахождение'
         });
         Provider.of<UserData>(context).currentUserId = signedInUser.uid;
         Navigator.pop(context);
