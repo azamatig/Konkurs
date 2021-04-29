aa3bc0a5445c59699954011d02479d29d193003e
victor-dm-black <vityadm199824@gmail.com>
2021-04-29 17:54:12 +0600

    removed unncessecary import

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_share_me/flutter_share_me.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
